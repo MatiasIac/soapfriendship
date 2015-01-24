b6686aab774838fd5f418e1af0a09f71c9484b9b
matiasiacono <matias@kinetica-solutions.com>
2015-01-24 05:40:31 -0300

    .

--- html/scripts/soap.js
+++ html/scripts/soap.js
@@ -1,25 +1,36 @@
 var soap = {
 	id: "soap",
 	visible: true,
-	x: 0, y: 0,
-	sizePorcentege: 1,
+	x: 11, y: 400,
+	sizePorcentege: 0.5,
+	soapSpeed: 1, soapDirection: 1,
+	soapTimer: {},
 	init: function () {
 		var self = this;
 		
 		this.particles1 = new cParticleEmitter();
 		this.particles1.init({ image: jsGFwk.ResourceManager.graphics.bubble.image});
-		this.particles1.position.y = 450;
-		this.particles1.position.x = 100;
-		this.particles1.positionRandom.x = 100;
-		this.particles1.positionRandom.y = 100;
-		this.particles1.maxParticles = 100;
-		this.particles1.size = 80;
+		this.particles1.position.y = this.y;
+		this.particles1.position.x = this.x;
+		this.particles1.positionRandom.x = 0;
+		this.particles1.maxParticles = 20;
+		this.particles1.size = 10;
 		this.particles1.sizeRandom = 5;
-		this.particles1.angle = 0;
-		this.particles1.gravity.x = 0;
-		this.particles1.lifeSpan = 4;
+		this.particles1.lifeSpan = 0.5;
+		
+		this.soapTimer = new jsGFwk.Timer({
+			action: function () {
+			   self.x += (self.soapSpeed * self.soapDirection);
+			   if (self.x > 600 || self.x < 10) {
+				self.soapDirection *= -1;
+			   }
+			}, 
+			tickTime: 0.01
+		});
 	},
 	update: function (delta) {
+		this.soapTimer.tick(delta);
+		this.particles1.position.x = this.x;
 		this.particles1.update(delta);
 	},
 	draw: function (context) {
