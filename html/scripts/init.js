ae917f27ff7f37c1af424cb9c5a3385cee7fe087
Patricio Marrone <marrone.patricio@gmail.com>
2015-01-23 23:03:39 -0300

    Moving init script to its own file

---
+++ html/scripts/init.js
@@ -0,0 +1,42 @@
+/*global document, jsGFwk*/
+document.onload = function init() {
+  jsGFwk.settings.canvas = "canvas";
+  jsGFwk.settings.clearColor = "rgb(50, 50, 50)";
+  jsGFwk.settings.frameRate = 1000 / 60;
+
+  jsGFwk.include("FastAnimation");
+  jsGFwk.include("Container");
+  jsGFwk.include("IO");
+  jsGFwk.include("Collisions");
+  jsGFwk.include("ResourceManager");
+  jsGFwk.include("Sprites");
+  jsGFwk.include("Fonts");
+  jsGFwk.include("Scenes");
+  jsGFwk.include("Path");
+
+  jsGFwk.Fonts.createFont({
+    name: 'zxBold',
+    source: 'fonts/zxBold.ttf'
+  });
+
+  jsGFwk.createObject({
+    id: "progress",
+    visible: true,
+    barWidth: 0,
+    init: function () {
+      jsGFwk.ResourceManager.onResourcesLoadedCompleted = function () {
+
+        //jsGFwk._gameObjects.progress.destroy();
+      };
+    },
+    update: function (delta) {},
+    draw: function (context) {
+      context.save();
+      context.fillStyle = "white";
+      context.fillText("Connecting..." + parseInt((jsGFwk.ResourceManager._totalLoadedResources * 100) / jsGFwk.ResourceManager._totalResources) + "%", 30, 40);
+      context.restore();
+    }
+  });
+
+  jsGFwk.start();
+};
